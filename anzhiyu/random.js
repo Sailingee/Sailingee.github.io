0b74dd24ca80f76b0e33ebb5e2db3e46b79723d0
Sailingee <957365721@qq.com>
2025-06-26 18:10:46 +0800

    Site updated: 2025-06-26 18:10:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/26/golang使用swagger/","2023/01/01/hello world/","2025/06/26/hello-world/"];function toRandomPost(){
+var posts=["2023/01/01/hello world/","2025/06/26/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
